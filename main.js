0887f9d78c2b56b9c24ffbdbee14428eb1f3120a
AvhikBiswas <abhikbiswas351@gmail.com>
2024-03-04 19:52:55 +0530

    style:back type Speed

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var typed = new Typed('.text', {
     strings: ["Full-Stack Developer ", " And a Youtuber"],
     typeSpeed: 100,
-    backSpeed: 300,
+    backSpeed: 120,
     backDelay: 1000,
     loop: true
 
